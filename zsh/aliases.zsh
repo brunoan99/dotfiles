07b854e30fe1b386802e0c28aeb0d091fc319b46
Bruno Andrade <brunoan99@gmail.com>
2022-11-21 04:49:15 -0300

    add auto remove alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,13 +1,15 @@
 alias reload='. ~/.zshrc'
 
+alias remove_dep='sudo pacman -R $(sudo pacman -Qdtq)'
+
 # EXA aliases
 # general use
-alias ls='exa'                                                         # ls
-alias l='exa -lbF --git'                                               # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUSa --git --color-scale'  # all list
-alias lx='exa -lbhHigUSa --git --color-scale' # all + extended list
+alias ls='exa --icons'                                                         # ls
+alias l='ls -lbF --git'                                               # list, size, type, git
+alias ll='ls -lbGF --git'                                             # long list
+alias llm='ls -lbGF --git --sort=modified'                            # long list, modified date sort
+alias la='ls -lbhHigUSa --git --color-scale'  # all list
+alias lx='ls -lbhHigUSa --git --color-scale' # all + extended list
 
 # speciality views                                                     # one column, just names
-alias lt='exa --tree --level=3'                                        # tree
+alias lt='ls --tree --level=3'                                        # tree
