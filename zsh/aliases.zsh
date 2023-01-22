63e5e1302d91439433a5544f47a5e7b650696a2a
Bruno Andrade <brunoan99@gmail.com>
2023-01-22 19:45:10 -0300

    add mkcd alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,8 @@
 alias reload='. ~/.zshrc'
 
-alias remove_dep='sudo pacman -R $(sudo pacman -Qdtq)'
+# navigation
+mkcd() { mkdir -p "$1" && cd "$1"; }                                     # create directory and enter it 
 
-# EXA aliases
 # general use
 alias ls='exa --icons'                                                # ls
 alias l='ls -lbF --git'                                               # list, size, type, git
@@ -10,10 +10,7 @@ alias ll='ls -lbGF --git'                                             # long lis
 alias llm='ls -lbGF --git --sort=modified'                            # long list, modified date sort
 alias la='ls -lbhHigUSa --git --color-scale'                          # all list
 alias lx='ls -lbhHigUSa --git --color-scale' 			      # all + extended list
-
-# speciality views                                                    # one column, just names
 alias lt='ls --tree --level=3'                                        # tree
-alias any='echo "any"'
 
 # so
 alias suspendit='sudo sh -c "echo mem > /sys/power/state"'
