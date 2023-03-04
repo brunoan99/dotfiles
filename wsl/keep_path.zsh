fbc6e31f74a20bf84a7b7e608b1271d075c7c794
Bruno Andrade <brunoan99@gmail.com>
2023-03-04 17:19:00 -0300

    remove wsl options

--- wsl/keep_path.zsh
+++
@@ -1,5 +0,0 @@
-# keep path when terminal is split
-keep_current_path() {
-  printf "\e]9;9;%s\e\\" "$(wslpath -w "$PWD")"
-}
-precmd_functions+=(keep_current_path)
--- wsl/path.zsh
+++
@@ -1,8 +0,0 @@
-# add to path without duplications
-function add_to_path {
-    PATH=:$PATH
-    PATH=$1${PATH//:$1:/:}
-}
-
-# add vscode path from outside of wsl
-add_to_path "/mnt/c/Users/nomeq/AppData/Local/Programs/Microsoft VS Code/bin"
