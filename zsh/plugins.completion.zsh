d7cb19ac4cdd40be31439d02cf18769668f06cf9
Bruno Andrade <brunoan99@gmail.com>
2023-03-10 18:26:07 -0300

    add asdf to languages version control

--- zsh/plugins.completion.zsh
+++ zsh/plugins.completion.zsh
@@ -1,2 +1,3 @@
 source_if_exists $CONFIG/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 source_if_exists $CONFIG/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source_if_exists $HOME/.asdf/asdf.sh
