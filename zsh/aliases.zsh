9e89f6d3ed30262291d9fa57722dc49fba1e8b8b
Bruno Andrade <brunoan99@gmail.com>
2023-10-17 19:40:36 -0300

    refactor: remove nvim configs and fix docker alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,7 +18,7 @@ alias dc='docker compose'
 alias dcr='docker compose down && docker compose up -d'
 alias remove_containers='docker rm -f "$(docker ps -aq)"'
 alias clean_containers='docker container prune'
-alias clean_images='docker image prune -all'
+alias clean_images='docker image prune'
 
 # cargo
 alias cargo-clippy='cargo clippy -- -A clippy::all -W clippy::correctness -W clippy::suspicious -W clippy::complexity -W clippy::perf -W clippy::style -W clippy::pedantic -W clippy::nursery -W clippy::cargo'
