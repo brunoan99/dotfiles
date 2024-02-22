b5fe451ef7dc2875cbf732f47a1f09b169d2ffcc
Bruno Andrade <brunoan99@gmail.com>
2024-02-22 17:19:29 -0300

    feat: improve history config

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -7,27 +7,33 @@ autoload -U $DOT/functions/*(:t)
 autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
 
-HISTFILE=~/.zsh_history
-HISTFILESIZE=
-HISTSIZE=10000
-
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt IGNORE_EOF
 
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
+###########
+# HISTORY #
+###########
 
-setopt HIST_FIND_NO_DUPS
+HISTFILE=$HOME/.zsh_history
+HISTSIZE=100000
+SAVEHIST=100000
+
+setopt INC_APPEND_HISTORY # imediately append to histoty file
+setopt SHARE_HISTORY  # share it across sessions
+setopt EXTENDED_HISTORY # record timestamp in history
+setopt HIST_EXPIRE_DUPS_FIRST # expire duplicate entires first when trimming history
+setopt HIST_IGNORE_DUPS # don't record and entry that was just recorded again
+setopt HIST_IGNORE_ALL_DUPS # delete old recorded entry if new entry is a duplicate
+setopt HIST_REDUCE_BLANKS # remove blanks from entries
+setopt HIST_FIND_NO_DUPS # don't display a line previously found
+setopt HIST_IGNORE_SPACE # remove entries when first char is a space
+setopt HIST_SAVE_NO_DUPS # don't write duplicate entries in the history file 
+unsetopt HIST_VERIFY # execute commands using history (e.g.: using !$) immediately
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
