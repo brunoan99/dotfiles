8dde863c2e86d4b2aa9379dd65b458064a6b9d97
Bruno Andrade <brunoan99@gmail.com>
2024-04-01 18:36:13 -0300

    feat: add some nvim options and packages to start on bootstrap

--- nvim/config/lua/vim-options.lua
+++ nvim/config/lua/vim-options.lua
@@ -1,5 +1,12 @@
+-- Line Numbers
 vim.wo.number = true
 vim.wo.relativenumber = true
 
 -- Dismiss Noice Message
 vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" })
+
+-- Vim options
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
